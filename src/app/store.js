53d20296a6645d42c3c2672ea29a750c0344dbc6
Quang Huy <vuquanghuy978@gmail.com>
2021-05-27 22:21:22 +0700

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterDateReducer from "../Features/people/components/peopleMainView/components/headerFilterDate/filterDateSlice"
 import candidatesReducer from "../Features/people/components/peopleMainView/components/mainCandidatesView/mainCandidatesSlice"
-import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 
 const rootReducer ={
@@ -10,6 +9,6 @@ const rootReducer ={
 }
 const store = configureStore({
     reducer:rootReducer,
-    middleware: [thunk, logger]
+    middleware: [thunk]
 })
 export default store
